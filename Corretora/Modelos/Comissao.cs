3bda156e5f0a45e1fbb77cad45a9ac7456991b13
flavio barbosa rodrigues <rodriguesflavio1@hotmail.com>
2019-03-03 08:50:50 -0300

    Criando estrutura de camadas do sistema corretora

---
+++ Corretora/Modelos/Comissao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Comissao
+    {
+        public int Id { get; set; }
+        public DateTime DataCarga { get; set; }
+        public double ValorTotalCarga { get; set; }
+    }
+}
---
+++ Corretora/Modelos/Conveniado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Conveniado
+    {
+        public int Nome { get; set; }
+        public string Nascimento { get; set; }
+    }
+}
